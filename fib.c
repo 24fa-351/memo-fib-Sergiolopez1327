1c45a123e44c143ffa898acfa5c8fb0015624355
Sergiolopez1327 <Sergiolopez132799@csu.fullerton.edu>
2024-09-23 22:28:33 -0700

    Fixed Memo Code

--- fib.c
+++ fib.c
@@ -4,16 +4,13 @@
 
 
 unsigned long long fib_i_core(int fibIndex) {
-    if (fibIndex == 0) {
-        return 0; 
-    } 
-    if (fibIndex == 1) {
-        return 1;  
-    } 
+    if (fibIndex <= 2) {
+        return fibIndex - 1;
+    }
     unsigned long long previous_num = 0;
     unsigned long long current_num = 1;
     unsigned long long next_num;
-    for (int index = 2; index <= fibIndex; index++) {
+    for (unsigned long long iter = 3; iter <= fibIndex; iter++) {
         next_num = previous_num + current_num;
         previous_num = current_num;
         current_num = next_num;
@@ -21,72 +18,57 @@ unsigned long long fib_i_core(int fibIndex) {
     return current_num;
 }
 unsigned long long fib_r_core(int fibIndex) {
-    if (fibIndex == 0) {
-        return 0; 
-    } 
-    if (fibIndex == 1) {
-        return 1;  
-    } 
+    if (fibIndex <= 2) {
+        return fibIndex - 1;
+    }
+
     return fib_r_core(fibIndex - 1) + fib_r_core(fibIndex - 2);
 }
-
-unsigned long long *memo_cache;
-
-unsigned long long memoized_fib_r_core(int fibIndex) {
+unsigned long long *memo_cache = NULL;
+unsigned long long fib_memo(int fibIndex, unsigned long long (*fib_func)(int)) {
+    memo_cache = (unsigned long long *)malloc((fibIndex + 1) * sizeof(unsigned long long));
+    for (int i = 0; i <= fibIndex; i++) {
+        memo_cache[i] = -1;
+    }
     if (memo_cache[fibIndex] != -1) {
         return memo_cache[fibIndex];
     }
-
     if (fibIndex == 0) {
         memo_cache[fibIndex] = 0;
     } else if (fibIndex == 1) {
-        memo_cache[fibIndex] = 1;
+        memo_cache[fibIndex] = 0;
     } else {
-        memo_cache[fibIndex] = memoized_fib_r_core(fibIndex - 1) + memoized_fib_r_core(fibIndex - 2);
+        memo_cache[fibIndex] = fib_func(fibIndex);
     }
     return memo_cache[fibIndex];
 }
-
-unsigned long long fib_wrapper(int fibIndex, char fib_method) {
-    if (fib_method == 'r') {
-        return fib_r_core(fibIndex);
-    } else if (fib_method == 'i') {
-        return fib_i_core(fibIndex);
-    }
-    return 0;
+unsigned long long fib_i(int fibIndex) {
+    return fib_memo(fibIndex, fib_i_core);
 }
-
-unsigned long long memoized_fib_wrapper(int fibIndex, char fib_method) {
-    if (fib_method == 'r') {
-        return memoized_fib_r_core(fibIndex);
-    } else if (fib_method == 'i') {
-        return fib_i_core(fibIndex);
-    }
-    return 0;
+unsigned long long fib_r(int fibIndex) {
+    return fib_memo(fibIndex, fib_r_core);
 }
-
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <integer> <r|i>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <integer> <method>\n", argv[0]);
         return 1;
     }
-
-    int fibIndex = atoi(argv[1]);
+    
+    int input_number = atoi(argv[1]);
     char fib_method = argv[2][0];
     unsigned long long result;
-
-    if (fib_method == 'r') {
-        memo_cache = (unsigned long long *)malloc((fibIndex + 1) * sizeof(unsigned long long));
-        for (int i = 0; i <= fibIndex; i++) {
-            memo_cache[i] = -1;
-        }
-        result = memoized_fib_wrapper(fibIndex - 1, fib_method);
-        free(memo_cache);
+    if (fib_method == 'i') {
+        result = fib_i(input_number);
+    } else if (fib_method == 'r') {
+        result = fib_r(input_number);
     } else {
-        result = fib_wrapper(fibIndex - 1, fib_method);
+        fprintf(stderr, "Invalid method. Use 'i' for iterative or 'r' for recursive.\n");
+        return 1;
     }
-
     printf("%llu\n", result);
-
+    if (memo_cache != NULL) {
+        free(memo_cache);
+        memo_cache = NULL;
+    }
     return 0;
 }
