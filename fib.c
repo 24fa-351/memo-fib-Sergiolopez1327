926fd62eea9380a3539b854fa027e32bce695e8b
Sergiolopez1327 <Sergiolopez132799@csu.fullerton.edu>
2024-09-28 16:52:12 -0700

    Fixed Memoization

--- fib.c
+++ fib.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <limits.h>
+
+#define MAX 1000
+#define MAX_FIB_VALUE ULLONG_MAX
+
+typedef unsigned long long (*FibMethod)(int);
+
+unsigned long long int memo_cache[MAX] = {0};
+
+FibMethod fibroutine;
+
+
+
+int fibOverflow(unsigned long long a, unsigned long long b) {
+    return (a > (ULLONG_MAX - b));
+}
 
 unsigned long long fib_i_core(int fibIndex) {
     if (fibIndex <= 2) {
@@ -10,67 +25,95 @@ unsigned long long fib_i_core(int fibIndex) {
     unsigned long long current = 1;
     unsigned long long next;
     for (unsigned long long iter = 3; iter <= fibIndex; iter++) {
+        if (fibOverflow(previous, current)) {
+            fprintf(stderr, "Overflow detected at index %llu\n", iter + 2);
+            return -1;
+        }
         next = previous + current;
         previous = current;
         current = next;
     }
     return current;
 }
+
 unsigned long long fib_r_core(int fibIndex) {
     if (fibIndex <= 2) {
         return fibIndex - 1;
     }
+    unsigned long long firstfibvalue = (*fibroutine)(fibIndex - 1);
+    unsigned long long secondfibvalue = (*fibroutine)(fibIndex - 2);
+    if (fibOverflow(firstfibvalue, secondfibvalue)) {
+        fprintf(stderr, "Overflow detected at index %d\n", fibIndex);
+        return -1;
+    }
+    return firstfibvalue + secondfibvalue;
+}
 
-    return fib_r_core(fibIndex - 1) + fib_r_core(fibIndex - 2);
-}
-unsigned long long* memo_cache = NULL;
-unsigned long long fib_memo(int fibIndex, unsigned long long (*fib_func)(int)) {
-    memo_cache = (unsigned long long*)malloc((fibIndex + 1) *
-                                             sizeof(unsigned long long));
-    for (int i = 0; i <= fibIndex; i++) {
-        memo_cache[i] = -1;
-    }
-    if (memo_cache[fibIndex] != -1) {
-        return memo_cache[fibIndex];
-    }
-    if (fibIndex == 0) {
-        memo_cache[fibIndex] = 0;
-    } else if (fibIndex == 1) {
-        memo_cache[fibIndex] = 0;
-    } else {
-        memo_cache[fibIndex] = fib_func(fibIndex);
-    }
-    return memo_cache[fibIndex];
-}
-unsigned long long fib_i(int fibIndex) {
-    return fib_memo(fibIndex, fib_i_core);
-}
-unsigned long long fib_r(int fibIndex) {
-    return fib_memo(fibIndex, fib_r_core);
-}
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <integer> <method>\n", argv[0]);
-        return 1;
-    }
+// unsigned long long fib_r(int fibIndex) {
+//     if (memo_cache[fibIndex] != 0) {
+//         return memo_cache[fibIndex];
+//     }
+//         memo_cache[fibIndex] = fib_r_core(fibIndex);
 
-    int input_number = atoi(argv[1]);
-    char fib_method = argv[2][0];
-    unsigned long long result;
-    if (fib_method == 'i') {
-        result = fib_i(input_number);
-    } else if (fib_method == 'r') {
-        result = fib_r(input_number);
+//     return memo_cache[fibIndex];
+
+// }
+
+// unsigned long long fib_i(int fibIndex) {
+//     if (memo_cache[fibIndex] != 0) {
+//         return memo_cache[fibIndex];
+//     }
+    
+//     memo_cache[fibIndex] = fib_i_core(fibIndex);
+    
+//     return memo_cache[fibIndex];
+
+// }
+FibMethod real_provider;
+
+
+ unsigned long long int fib_memo(int fibIndex) {
+    if (memo_cache[fibIndex] != 0)
+      return memo_cache[fibIndex];
+
+
+    if (fibIndex <= 2) {
+       memo_cache[fibIndex] = fibIndex - 1;
+       return memo_cache[fibIndex];
     } else {
-        fprintf(
-            stderr,
-            "Invalid method. Use 'i' for iterative or 'r' for recursive.\n");
-        return 1;
+     memo_cache[fibIndex] = (*real_provider)(fibIndex);
     }
-    printf("%llu\n", result);
-    if (memo_cache != NULL) {
-        free(memo_cache);
-        memo_cache = NULL;
-    }
-    return 0;
+   return memo_cache[fibIndex];
+ }
+
+
+int main(int argc, char *argv[]) {
+
+   if (argc != 3 || (argv[2][0] != 'r' && argv[2][0] != 'i')) {
+      printf("Invalid Input\n");
+      return 0;
+   }
+
+   int input_number = atoi(argv[1]);
+   char fib_method = argv[2][0];
+   unsigned long long int result;
+
+   fibroutine = fib_memo;
+   
+   if (fib_method == 'r') {
+      real_provider = fib_r_core;
+   }
+   else if (fib_method == 'i') {
+      real_provider = fib_i_core;
+   } else {
+       fprintf(stderr, "Invalid method. Use 'i' for iterative or 'r' for recursive.\n");
+       return 1;
+   }
+
+   fibroutine = fib_memo;
+
+   result = (*fibroutine)(input_number);
+   
+   printf("%llu\n", result);
+   return 0;
 }
